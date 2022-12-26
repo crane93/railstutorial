76e9f2ab789a0ccaab6bbb53d980aa3e0ed7d35e
crane93 <youl9094@gmail.com>
2022-12-26 10:45:38 +0000

    hola, mundo!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
     render html:"¡Hola, mundo!"
   end
   def goodbye
-    render html:"goodbye world😢"
+    render html:"hola, mundo!"
   end
 end
